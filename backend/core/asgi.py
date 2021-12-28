51c18560fc6a8faa7a399682396b4b93a2bcc391
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2021-12-28 12:57:47 +0600

    Update asgi.py

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -17,7 +17,7 @@ from core.routing import websocket_urlpatterns
 from django.core.asgi import get_asgi_application
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.dev")
 
 application = ProtocolTypeRouter(
     {
